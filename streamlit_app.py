23e7504263c7afdbdd124b9404f4e5b8501f6a6f
Shashikant <52612360+shashikantdev3@users.noreply.github.com>
2024-02-01 07:31:57 +0530

    Update streamlit_app.py
    This change is to deploy the app on render

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,5 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import plotly.offline as pyo
-import plotly.graph_objects as go
 import warnings
 
 
